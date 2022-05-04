c6edfa913fa8abe8a68225875312d427c7719806
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-05-04 02:16:43 -0500

    proposal for new activity

--- mecurySedan.js
+++
@@ -1,6 +0,0 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
-
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
--- vehicle.js
+++ vehicle.js
@@ -85,7 +85,3 @@ class Vehicle {
     }
 }
 
-//This exports things you want to use from this "module", more info in readme
-module.exports = {
-    Vehicle
-}
