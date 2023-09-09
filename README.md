09eceec158c1a7b5a0f67c7fbc37b8d5178d70d5
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2023-09-09 14:47:30 -0500

    better instructions

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Activity Automative
 
-- In this lesson students work on sub-classing a base vehicle class
-- to do this we need a way of importing / using code from a different file...
+- In this lesson students work on creating a vehicle and factory class
+- the learning objective is applying the concepts of classes to solve a real-world problem of modeling a vehicle manufacturing factory
 
-[Official Node.js docs](https://nodejs.org/docs/v0.4.2/api/modules.html#all_Together...)
+## Instructions
+- create a `Vehicle` class in the vehicle.js file that adheres to the provided class diagram
+- a vehicle should by default have an `undefined` paint, since all metal starts unpainted
+- when the `.paint("blue")` method is called on a vehicle it should paint it blue
+- create a `Factory` class in the factory.js file that adheres to the provided class diagram
+
+
+## Acceptance criteria
+- The vehicle class passes all tests
+- The vehicle class definition matches the class diagram
+- The factory class passes all tests
+- The factory class definition matches the class diagram
+
 
--- tests.js
+++ tests.js
@@ -1,20 +1,69 @@
-let baseClassTestElement = document.querySelector("#base-class-test")
-let derivedClassTestElement = document.querySelector("#derived-class-test")
-let appliedClassTest1Element = document.querySelector("#applied-class-test-1")
-let appliedClassTest2Element = document.querySelector("#applied-class-test-2")
-baseClassTestElement.style.color = "red"
-derivedClassTestElement.style.color = "red"
-appliedClassTest1Element.style.color = "red"
-appliedClassTest2Element.style.color = "red"
+let vehicleClassTestElement = document.querySelector("#vehicle-class-test")
+let factoryClassTestElement = document.querySelector("#factory-class-test")
+
+vehicleClassTestElement.style.color = "red"
+factoryClassTestElement.style.color = "red"
+
 //wrap everything in try catch since they won't have classes to begin with
 try {
     //Base Class tests basically just say they didn't break anything
-    let v = new Vehicle("Jord J-051")
-    if(v.model === "Jord J-051" && v.drive() === "Jord J-051 drives"){
-        baseClassTestElement.textContent = "Passed"
-        baseClassTestElement.style.color = "green"
+    let v = new Vehicle("Jord", "J-051", 2015, 50000, "12312")
+    v.paint("blue")
+    console.log("Test case 1 vehicle: ", v)
+    console.log("Test conditions", 
+        v.make === "Jord" ,
+        v.model === "J-051",
+        v.year === 2015 ,
+        v.price === 50000 ,
+        v.vin === "12312" ,
+        v.color === "blue" ,
+        v.drive() === "2015 Jord J-051 drives"
+    )
+    if(
+        v.make === "Jord" &&
+        v.model === "J-051" && 
+        v.year === 2015 &&
+        v.price === 50000 &&
+        v.vin === "12312" &&
+        v.color === "blue" &&
+        v.drive() === "2015 Jord J-051 drives"
+    ){
+        vehicleClassTestElement.textContent = "Passed"
+        vehicleClassTestElement.style.color = "green"
     }
 } catch (error) {
     
-    console.log("some base class tests have failed error:", error)
+    console.log("some vehicle class tests have failed error:", error)
+}
+
+try {
+    //factory Class tests 
+    let f = new Factory("Germany", "Elon Tusk")
+    f.createVehicle(new Vehicle("Ford", "F-150",2018, 19823,"123123"))
+    f.paintVehicle("123123", "green")
+    f.createVehicle(new Vehicle("Chevy", "Silverado",2019, 11234,"98081"))
+    f.paintVehicle("98081", "red")
+    console.log("Test case 2 factory: ", f)
+    console.log("Test conditions", 
+        f.location === "Germany" ,
+        f.owner === "Elon Tusk",
+        f.vehicles.length === 2 ,
+        f.vehicles.find(v => v.vin === "123123").color === 'green',
+        f.vehicles.find(v => v.vin === "98081").color === 'red' ,
+        f.listVehicles() === f.vehicles
+    )
+    if(
+        f.location === "Germany" &&
+        f.owner === "Elon Tusk"&&
+        f.vehicles.length === 2 &&
+        f.vehicles.find(v => v.vin === "123123").color === 'green'&&
+        f.vehicles.find(v => v.vin === "98081").color === 'red' &&
+        f.listVehicles() === f.vehicles
+    ){
+        factoryClassTestElement.textContent = "Passed"
+        factoryClassTestElement.style.color = "green"
+    }
+} catch (error) {
+    
+    console.log("some factory class tests have failed error:", error)
 }
